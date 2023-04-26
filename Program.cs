59467f70aa9339a4226b97db43b78459ad1a7c33
aud404_st <aud404_st@CR404-PC18>
2023-04-26 17:13:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть три параметри: два числа і дію над ними");
+        string a = "";
+        double x = 0;
+        double y = 0;
+        if (args.Length != 0)
+        {
+            a = args[1];
+            x = double.Parse(args[0]);
+            y = double.Parse(args[2]);
+
+            switch (a)
+            {
+                case "+":
+                    {
+                        Console.WriteLine(x + "+" + y + "=" + (x + y));
+                        break;
+                    }
+                case "-":
+                    {
+                        Console.WriteLine(x + "-" + y + "=" + (x - y));
+                        break;
+                    }
+                case "/":
+                    {
+                        if (y != 0)
+                            Console.WriteLine(x + "/" + y + "=" + (x / y));
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine(x + "*" + y + "=" + (x * y));
+                        break;
+                    }
+                case "%":
+                    {
+                        Console.WriteLine(x + "%" + y + "=" + (x % y));
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Ви ввели некоректний символ!");
+                        break;
+                    }
+            }
+        }
+    }
+}
